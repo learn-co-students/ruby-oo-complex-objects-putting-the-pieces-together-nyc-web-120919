8d351d83903dc2ea220a1bd4540c66ade1c0a35b
Jay Wen <jaywen89@gmail.com>
2019-12-10 19:05:44 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,31 +1,14 @@
 # Make your shoe class here!
 class Shoe
  # alternative
- # attr_accessor :color, :size, :material, :condition
+ attr_accessor :color, :size, :material
   def initialize(brand)
     @brand = brand
   end
   def brand
     @brand
   end
-  def color=(color)
-    @color = color
-  end
-  def color
-    @color
-  end
-  def size=(size)
-    @size = size
-  end
-  def size
-    @size
-  end
-  def material=(material)
-    @material = material
-  end
- def material
-  @material
-end
+
 def condition=(condition)
   @condition= condition
 end
